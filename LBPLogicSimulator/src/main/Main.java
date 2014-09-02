7414ab0521bd9b664ede94535b4a029c23eb8be1
Coolway99 <xxcoolwayxx@gmail.com>
2014-09-01 23:10:29 -0400

    Created update timer

--- LBPLogicSimulator/src/main/Main.java
+++ LBPLogicSimulator/src/main/Main.java
@@ -1,12 +1,25 @@
 package main;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 
 public class Main {
-	public static final JFrame mainFrame = new MainFrame();
+	public static final String synchronizationKeyA = "bleh";
+	public static final MainFrame mainFrame = new MainFrame();
 	public static void main(String[] Args){
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainFrame.setVisible(true);
+		Timer timer = new Timer(true);
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				Main.update();
+			}
+		}, 1000/30);
+	}
+	public static void update(){
+		
 	}
 }
