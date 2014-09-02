6fccfc786802d1a3764ce4f55964eb30030027bd
Coolway99 <xxcoolwayxx@gmail.com>
2014-09-01 23:19:59 -0400

    Port was unneeded

--- LBPLogicSimulator/src/main/interfaces/Signal.java
+++ LBPLogicSimulator/src/main/interfaces/Signal.java
@@ -1,16 +1,11 @@
 package main.interfaces;
 
 public class Signal {
-	int port;
 	boolean digital;
 	double analog;
-	public Signal(int port, boolean digital, double analog){
-		this.port = port;
+	public Signal(boolean digital, double analog){
 		this.digital = digital;
 		this.analog = analog;
-	}
-	public int getPort(){
-		return port;
 	}
 	public boolean getDigital(){
 		return digital;
